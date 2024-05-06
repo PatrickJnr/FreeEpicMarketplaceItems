004cb31e4676470379554b620b1a5f05d0322f92
Patrick Jr <patrickcaulton423@gmail.com>
2024-05-06 11:05:30 +0100

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){var e=document.querySelectorAll(".btn.add-btn.add-cart-btn.shift-left"),t=0,n=setInterval(function(){if(t<e.length){var s=e[t].closest(".asset").querySelector(".asset-price");"Free"===(s?s.innerText.trim():%22%22)&&e[t].click(),t++}else%20clearInterval(n)},500)})();
