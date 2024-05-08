f17b62dc7e22ebe560a6b3adc190855311faec23
Patrick Jr <patrickcaulton423@gmail.com>
2024-05-08 23:19:16 +0100

    Update bookmarklet.js
    forgot "javascript:" part

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-!function(){var t=document.querySelectorAll(".btn.add-btn.add-cart-btn.shift-left"),n=0,e=setInterval((function(){n<t.length?(t[n].click(),n++):clearInterval(e)}),500)}();
+javascript:!function(){var t=document.querySelectorAll(".btn.add-btn.add-cart-btn.shift-left"),n=0,e=setInterval((function(){n<t.length?(t[n].click(),n++):clearInterval(e)}),500)}();
