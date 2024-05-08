0def629068ebf1152677f086537fb2707903d11b
Patrick Jr <patrickcaulton423@gmail.com>
2024-05-08 23:16:59 +0100

    Update unmin.js

--- unmin.js
+++ unmin.js
@@ -1,10 +1,12 @@
-javascript: (function() {
+(function(){
     var e = document.querySelectorAll(".btn.add-btn.add-cart-btn.shift-left"),
         t = 0,
-        n = setInterval(function() {
-            if (t < e.length) {
-                var s = e[t].closest(".asset").querySelector(".asset-price");
-                "Free" === (s ? s.innerText.trim() : % 22 % 22) && e[t].click(), t++
-            } else % 20 clearInterval(n)
-        }, 500)
+        n = setInterval(function(){
+            if(t < e.length){
+                e[t].click();
+                t++;
+            } else {
+                clearInterval(n);
+            }
+        }, 500);
 })();
