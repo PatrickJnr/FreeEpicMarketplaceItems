222ff152ee3cbf7585170a6d0dc13f8e47bdde9f
Patrick Jr <patrickcaulton423@gmail.com>
2024-05-08 23:20:14 +0100

    Update unmin.js
    forgot javascript:

--- unmin.js
+++ unmin.js
@@ -1,12 +1,7 @@
-(function(){
-    var e = document.querySelectorAll(".btn.add-btn.add-cart-btn.shift-left"),
-        t = 0,
-        n = setInterval(function(){
-            if(t < e.length){
-                e[t].click();
-                t++;
-            } else {
-                clearInterval(n);
-            }
+javascript: !(function () {
+    var t = document.querySelectorAll(".btn.add-btn.add-cart-btn.shift-left"),
+        n = 0,
+        e = setInterval(function () {
+            n < t.length ? (t[n].click(), n++) : clearInterval(e);
         }, 500);
 })();
